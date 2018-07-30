ab139d847fe88a9bff1bd2addd2749132895a986
gyd <gongyidong@gongyidongdeMacBook-Pro.local>
2018-07-30 13:37:59 +0800

    去掉valid_server配置文件

---
+++ deploy/valid_server/config.sample.php
@@ -0,0 +1,13 @@
+<?php 
+return array (
+  'remote_servers' => 
+  array (
+    0 => 
+    array (
+      'name' => '',
+      'host' => '',
+      'user' => '',
+    ),
+  ),
+  'identity_file_path' => '',
+);
